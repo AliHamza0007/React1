db40e792c1fa375cd3cd7c0fa9643adf8d30d296
AliHamza0007 <hamzasarwer9@gmail.com>
2023-10-11 19:51:05 +0500

    kyggujik

---
+++ src/Pages/Counter.js
@@ -0,0 +1,89 @@
+<<<<<<< HEAD
+import React, { useState } from "react";
+import Layout from "../commponents/Layout";
+var timer = undefined;
+const Counter = () => {
+  const [counter, setcounter] = useState(0);
+  const [started, setstarted] = useState(false);
+
+  let start = () => {
+    timer = setInterval(() => {
+      setcounter((pre) => pre + 1);
+      setstarted(true);
+    }, 1000);
+  };
+  let stop = () => {
+    clearInterval(timer);
+    setstarted(false);
+  };
+  let reset = () => {
+    setcounter(0);
+    clearInterval(timer);
+    setstarted(false);
+  };
+  return (
+    <Layout>
+      <div className="text-center text-white mt-5">
+        <div className="h3">Timer </div>
+        <h1>{counter}</h1>
+        <button
+          disabled={started}
+          onClick={start}
+          className="btn btn-success m-2"
+        >
+          Start
+        </button>
+        <button onClick={stop} className="btn btn-danger m-2">
+          Stop
+        </button>
+        <button onClick={reset} className="btn btn-primary m-2">
+          Reset
+        </button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Counter;
+=======
+import React, { useState } from 'react'
+import Layout from '../commponents/Layout';
+    var timer = undefined;
+const Counter=()=> {
+
+    
+    const [counter, setcounter] = useState(0)
+    const [started, setstarted] = useState(false)
+    
+    let start = () => {
+        timer = setInterval(() => {
+            setcounter((pre) => pre + 1)
+            setstarted(true)
+        }, 1000);
+    }
+    let stop = () => {
+        clearInterval(timer)
+        setstarted(false)
+    }
+    let reset = () => {
+        setcounter(0)
+        clearInterval(timer)
+        setstarted(false)
+
+    }
+    return (
+        <Layout>
+
+        <div className='text-center text-white mt-5'>
+            <div className='h3'>Timer </div>
+            <h1>{counter}</h1>
+            <button disabled={started} onClick={start} className="btn btn-success m-2">Start</button>
+            <button onClick={stop} className="btn btn-danger m-2">Stop</button>
+            <button onClick={reset} className="btn btn-primary m-2">Reset</button>
+        </div>
+        </Layout>
+    )
+}
+
+export default Counter
+>>>>>>> 0389174572a41736029a47bca0f2bc08772a6627
